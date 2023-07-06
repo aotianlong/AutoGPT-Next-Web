3d54fd7b7a99217ea9d1056f7664482977cc647b
aotianlong <aotianlong@hengfirst.com>
2023-07-07 00:09:09 +0800

    feat: save

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const GPT_35_TURBO = "gpt-3.5-turbo" as const;
 export const GPT_4 = "gpt-4" as const;
-export const GPT_35_TURBO_16K = "gpt-3.5-turbo-16k" as const;
+export const GPT_35_TURBO_16K = "xy-openai-gpt35-16k" as const;
 export const GPT_MODEL_NAMES = [GPT_35_TURBO, GPT_35_TURBO_16K, GPT_4];
 
 export const DEFAULT_MAX_LOOPS_FREE = 4 as const;
